99aeafb93b0b7b313bde49c787a3b4c2f6ef9fe9
Gabriel Lennon <gabriel.lennon@ifood.com.br>
2024-09-10 18:20:55 -0300

    feature: table goals

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,29 @@
 import { pgTable, text, integer, timestamp } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+
+/*
+  Crio primeiro a tabela depois rodo npx drizzle-kit generate 
+  depois npx drizzle-kit migrate para gerar a migrate
+*/
 
 export const goals = pgTable('goals', {
-  id: text('id').primaryKey(),
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
   title: text('title').notNull(),
   desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+})
+
+export const goalCompletions = pgTable('goal_completions', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  goalId: text('goal_id')
+    .references(() => goals.id)
+    .notNull(),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
